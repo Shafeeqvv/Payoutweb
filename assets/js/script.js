1d4077fa07054e78d91e9879a497ee55710ac167
Shafeeqvv <shafeeqsharafi@gmail.com>
2024-12-04 16:53:26 +0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -698,6 +698,10 @@
 
 
 
-
+var mainDiv = document.getElementById('main-button');
+mainDiv.addEventListener('click', function(){
+  this.children.item(0).classList.toggle('fa-times');
+  this.classList.toggle('open');
+});
 
 
